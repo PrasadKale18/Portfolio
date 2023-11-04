9c4fb0919d8e75817b644b6f035217217018cc4b
PrasadKale18 <149289386+PrasadKale18@users.noreply.github.com>
2023-11-04 09:03:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Full Stack Developer"],
+        strings: ["Full Stack Developer","Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Full Stack Developer"],
+        strings: ["Full Stack Developer","Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
